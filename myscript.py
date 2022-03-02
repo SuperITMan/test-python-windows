330a1ccbf3eb352af95d2b5e213da5ecf3837215
Alexis Georges <admin@superitman.com>
2022-03-02 01:43:36 +0000

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import glob
 import os
-import path
+from os import path, remove
 import pandas as pd
 
 from driver_builder import DriverBuilder
